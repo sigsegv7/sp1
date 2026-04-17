56a793c39b206ba8e435f04850d1a7927fef68f3
Ian Moffett <ian@mirocom.org>
2026-04-16 22:34:25 -0400

    sp1: os: Print boot banner on startup
    Signed-off-by: Ian Moffett <ian@mirocom.org>

--- usr/src/sp1/common/os/main.c
+++ usr/src/sp1/common/os/main.c
@@ -12,15 +12,27 @@
 #include <io/cons/cons.h>
 #include <lib/printf.h>
 
+#define KERNEL_VERSION "0.0.1"
+
 /* Root console attribute */
 struct cons_attr cons_attr = {
     .fg = 0x808080,
     .bg = 0x000000
 };
 
+static void
+boot_banner(void)
+{
+    printf("-- sp1/amd64 %s --\n", KERNEL_VERSION);
+    printf("yiff!~ preparing syspaw for you!... <3\n");
+}
+
 void
 main(void)
 {
     /* Initialize the console */
     cons_init(&cons_attr);
+
+    /* Write the boot console */
+    boot_banner();
 }
