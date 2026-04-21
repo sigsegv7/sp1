250c44635b07a6d5dcacc9bff795482679472e5d
Ian Moffett <ian@mirocom.org>
2026-04-21 18:57:18 -0400

    head: sys: Add sys/mman.h
    Signed-off-by: Ian Moffett <ian@mirocom.org>

---
+++ usr/src/head/sys/mman.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2026, Mirocom Laboratories
+ * All rights reserved.
+ *
+ * The following sources are CONFIDENTIAL and PROPRIETARY
+ * property of Mirocom Laboratories. Unauthorized copying,
+ * use, distribution or modification of this file, in whole
+ * and in part, is strictly prohibited without the prior written
+ * consent from Mirocom Laboratories.
+ */
+
+#ifndef _SYS_MMAN_H_
+#define _SYS_MMAN_H_ 1
+
+#include <sys/param.h>
+
+/* Protection flags */
+#define PROT_READ   0x00        /* Mapping is readable */
+#define PROT_WRITE  BIT(0)      /* Mapping is writable */
+#define PROT_EXEC   BIT(1)      /* Mapping is executable */
+
+#endif  /* !_SYS_MMAN_H_ */
