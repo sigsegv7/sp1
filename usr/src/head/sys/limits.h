0ca56a55d0cdbb9ce09b8db52b79e50374a363e4
Chloe M. <chloe@mirocom.org>
2026-06-01 03:25:00 -0500

    sys: Add sys/limits.h
    Signed-off-by: Chloe M. <chloe@mirocom.org>

---
+++ usr/src/head/sys/limits.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2026, Mirocom Laboratories
+ * All rights reserved.
+ *
+ * The following sources are CONFIDENTIAL and PROPRIETARY
+ * property of Mirocom Laboratories. Unauthorized copying,
+ * use, distribution or modification of this file, in whole
+ * and in part, is strictly prohibited without the prior written
+ * consent from Mirocom Laboratories.
+ */
+
+#ifndef _SYS_LIMITS_H_
+#define _SYS_LIMITS_H_ 1
+
+#define CHAR_BIT 8
+
+#endif  /* !_SYS_LIMITS_H_ */
