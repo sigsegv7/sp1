7eebb739ecda91c6763e87b364e323a4f860200b
Chloe Moffett <chloe@mirocom.org>
2026-06-04 16:15:52 +0000

    sp1: pci: Add PCI advertisement groundwork
    Signed-off-by: Chloe Moffett <chloe@mirocom.org>

--- usr/src/sp1/head/io/pci/pci.h
+++ usr/src/sp1/head/io/pci/pci.h
@@ -14,6 +14,37 @@
 
 #include <sys/status.h>
 #include <sys/types.h>
+#include <io/pci/device.h>
+
+/* Vendor/device pair */
+#define PCI_VEN_DEV(VENDOR_ID, DEVICE_ID)   \
+    {                                       \
+        .vendor_id = (VENDOR_ID),           \
+        .device_id = (DEVICE_ID),           \
+        .ven_dev = 1                        \
+    }
+
+/*
+ * Represents a PCI device advertisement
+ *
+ * @vendor_id:  Device vendor ID
+ * @device_id:  Device ID
+ * @ven_dev:    If set, using vendor/device pair
+ */
+struct pci_adv {
+    uint16_t vendor_id;
+    uint16_t device_id;
+    uint8_t ven_dev : 1;
+};
+
+/*
+ * Represents a PCI driver
+ *
+ * @kick: Initializes the hardware associated with the PCI device driver
+ */
+struct pci_driver {
+    status_t(*kick)(struct pci_device *dev);
+};
 
 /*
  * Initialize the PCI bus driver
