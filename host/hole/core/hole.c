d6c13de6f40dc1e53b40e32056bdf1b108f16de3
Chloe Moffett <chloe@mirocom.org>
2026-06-05 01:47:16 +0000

    host: hole: Implement initial bundling logic
    Signed-off-by: Chloe Moffett <chloe@mirocom.org>

--- host/hole/core/hole.c
+++ host/hole/core/hole.c
@@ -10,19 +10,26 @@
  */
 
 #define _DEFAULT_SOURCE
+#include <sys/mman.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/queue.h>
 #include <stdio.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <dirent.h>
 
+#define FILE_ALIGN 512
+#define ALIGN_UP(value, align)        (((value) + (align)-1) & ~((align)-1))
+
 #define REF_MAGIC "CUMHOLE"
 #define REF_MAGIC_LEN 8
+#define REF_PATH_LEN 256
 
 /* Set to 1 for verbose */
-#define DEBUG 1
+#define DEBUG 0
 
 #if DEBUG
 #define DTRACE(fmt, ...) printf(fmt, ##__VA_ARGS__)
@@ -36,18 +43,24 @@
  *
  * @magic:      Reference magic
  * @path:       File path
+ * @size:       File size
  * @data_off:   Data offset
  * @link:       Queue link
+ *
+ * XXX: Do not reorder anything ABOVE 'link'
  */
 struct hole_ref {
     char magic[REF_MAGIC_LEN];
-    char *path;
+    char path[REF_PATH_LEN];
+    size_t size;
     off_t data_off;
     TAILQ_ENTRY(hole_ref) link;
 };
 
 /* Globals */
+static const char *output_path = "out.hole";
 static char *fill_dirpath = NULL;
+static size_t file_count = 0;
 static TAILQ_HEAD(, hole_ref) hole_files;
 
 /*
@@ -72,11 +85,106 @@ hole_files_destroy(void)
     while ((ref = TAILQ_FIRST(&hole_files)) != NULL) {
         DTRACE("delete %s\n", ref->path);
 
-        free(ref->path);
         tmp = ref;
         TAILQ_REMOVE(&hole_files, ref, link);
         free(tmp);
     }
+}
+
+/*
+ * Used to fix up offsets after
+ */
+static void
+hole_table_fixup(void)
+{
+    struct stat sb;
+    struct hole_ref *ref;
+    size_t table_end, offset;
+
+    table_end = file_count * sizeof(struct hole_ref);
+    offset = table_end;
+
+    printf("* fixing up offsets...\n");
+    printf("* table end @ %zx\n", table_end);
+
+    TAILQ_FOREACH(ref, &hole_files, link) {
+        if (stat(ref->path, &sb) < 0) {
+            perror("stat");
+            continue;
+        }
+
+        ref->size = sb.st_size;
+        ref->data_off = ALIGN_UP(table_end + offset, FILE_ALIGN);
+
+        DTRACE("data_off=%zx\n", ref->data_off);
+        offset += ref->size;
+    }
+}
+
+static void
+blob_write(void)
+{
+    struct hole_ref *ref;
+    int fd, ffd;
+    void *data;
+    size_t fsize;
+
+    fd = open(output_path, O_RDWR | O_CREAT | O_TRUNC, 0777);
+    if (fd < 0) {
+        perror("open");
+        return;
+    }
+
+    /*
+     * PASS 1):
+     *      We are to write the header of each file into the
+     *      top of the blob.
+     */
+    TAILQ_FOREACH(ref, &hole_files, link) {
+        write(fd, ref->magic, sizeof(ref->magic));
+        write(fd, ref->path, strlen(ref->path) + 1);
+        write(fd, &ref->size, sizeof(ref->size));
+        write(fd, &ref->data_off, sizeof(ref->data_off));
+    }
+
+    /*
+     * PASS 2):
+     *      Now we can write the contents of each file after
+     */
+    TAILQ_FOREACH(ref, &hole_files, link) {
+        ffd = open(ref->path, O_RDONLY);
+        if (ffd < 0) {
+            perror("open[ffd]");
+            continue;
+        }
+
+        /* Obtain the file size */
+        fsize = lseek(ffd, 0, SEEK_END);
+        lseek(ffd, 0, SEEK_SET);
+
+        data = mmap(
+            NULL,
+            fsize,
+            PROT_READ,
+            MAP_SHARED,
+            ffd,
+            0
+        );
+
+        if (data == NULL) {
+            close(ffd);
+            perror("mmap");
+            continue;
+        }
+
+        lseek(fd, SEEK_SET, ref->data_off);
+        write(fd, data, fsize);
+        munmap(data, fsize);
+        close(ffd);
+    }
+
+    close(fd);
+    return;
 }
 
 /*
@@ -99,10 +207,11 @@ push_file(const char *path)
     }
 
     memcpy(ref->magic, REF_MAGIC, sizeof(REF_MAGIC));
-    ref->path = strdup(path);
-
+    memcpy(ref->path, path, strlen(path) + 1);
     ref->data_off = 0;
+
     TAILQ_INSERT_TAIL(&hole_files, ref, link);
+    ++file_count;
     return 0;
 }
 
@@ -134,6 +243,7 @@ scan_dir(const char *path)
         snprintf(pathbuf, sizeof(pathbuf), "%s/%s", path, dirent->d_name);
         switch (dirent->d_type) {
         case DT_REG:
+            printf("[f] %s\n", pathbuf);
             if (push_file(pathbuf) < 0) {
                 free(fill_dirpath);
                 exit(1);
@@ -183,6 +293,10 @@ main(int argc, char **argv)
 
     TAILQ_INIT(&hole_files);
     scan_dir(fill_dirpath);
+
+    hole_table_fixup();
+    blob_write();
+
     hole_files_destroy();
     free(fill_dirpath);
     return 0;
